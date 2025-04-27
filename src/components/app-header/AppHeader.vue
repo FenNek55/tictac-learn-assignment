c05828665094c8cd85d362cad64ac730c80212d6
Karol Stasik <stasik.karol@gmail.com>
2025-04-27 12:26:36 +0200

    Make header sticky

--- src/components/app-header/AppHeader.vue
+++ src/components/app-header/AppHeader.vue
@@ -1,22 +1,29 @@
 <template>
-  <v-container class="pt-6 mb-6" fluid>
-    <div class="app-header__content mb-4 mb-md-0">
-      <RouterLink to="/">
-        <v-img class="app-header__logo" src="@/assets/logo.svg" />
-      </RouterLink>
-      <ProductSearchbar class="app-header__searchbar-desktop d-none d-md-flex" />
-      <div class="app-header__right">
-        <AppHeaderCart />
-        <v-avatar image="@/assets/images/placeholder_avatar.jpg" size="42" />
+  <header class="app-header">
+    <v-container class="pt-6 mb-6" fluid>
+      <div class="app-header__content mb-4 mb-md-0">
+        <RouterLink to="/">
+          <v-img class="app-header__logo" src="@/assets/logo.svg" />
+        </RouterLink>
+        <ProductSearchbar class="app-header__searchbar-desktop d-none d-md-flex" />
+        <div class="app-header__right">
+          <AppHeaderCart />
+          <v-avatar image="@/assets/images/placeholder_avatar.jpg" size="42" />
+        </div>
       </div>
-    </div>
-    <ProductSearchbar class="app-header__searchbar-mobile d-flex d-md-none" />
-  </v-container>
-  <v-divider />
+      <ProductSearchbar class="app-header__searchbar-mobile d-flex d-md-none" />
+    </v-container>
+    <v-divider />
+  </header>
 </template>
 
 <style lang="scss" scoped>
 .app-header {
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  background-color: rgb(var(--v-theme-background));
+
   &__content {
     display: flex;
     gap: 24px;
