63a1509ee289eba7cb96293bee7546e9a97d8bde
Karol Stasik <stasik.karol@gmail.com>
2025-04-29 12:40:36 +0200

    chore: Update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,1 +1,18 @@
-export { default } from 'eslint-config-vuetify/index.ts.mjs'
+import vuetify from 'eslint-config-vuetify/index.ts.mjs'
+
+export default [
+  {
+    ...vuetify,
+    languageOptions: {
+      ...(vuetify.languageOptions || {}),
+      globals: {
+        ...(vuetify.languageOptions?.globals || {}),
+        IntersectionObserver: 'readonly',
+        IntersectionObserverInit: 'readonly',
+        HTMLElement: 'readonly',
+        window: 'readonly',
+        document: 'readonly',
+      },
+    },
+  },
+]
