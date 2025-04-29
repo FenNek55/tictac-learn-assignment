de012f1527ddbe064166f5480a0120865d0aeab6
Karol Stasik <stasik.karol@gmail.com>
2025-04-29 12:50:12 +0200

    fix: Update useIntersectionObserver composable func declaration

--- src/composables/useIntersectionObserver.ts
+++ src/composables/useIntersectionObserver.ts
@@ -1,9 +1,9 @@
 import { onMounted, onUnmounted, ref } from 'vue';
 
-export function useIntersectionObserver (
+export const useIntersectionObserver = (
   callback: () => void,
   options: IntersectionObserverInit = { root: null, rootMargin: '0px', threshold: 0 }
-) {
+) => {
   const target = ref<HTMLElement | null>(null);
   let observer: IntersectionObserver | null = null;
 
